f4e6a29b15d4e2b17fb09ead44fea31c818aaa08
mozzanovsemen-droid <mozzanov.semen@gmail.com>
2026-02-19 22:02:57 +0800

    Удалено

--- schemas.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import Optional
-
-class STaskAdd(BaseModel):
-    name: str
-    description: Optional[str] = None
-
-class STask(STaskAdd):
-    id: int
